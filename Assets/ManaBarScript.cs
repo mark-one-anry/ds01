b7532ade26426885373a30639e1425c77b600256
mark-one-anry <deadok2000@yandex.ru>
2021-08-22 12:48:38 +0300

    player prefab

---
+++ Assets/ManaBarScript.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ManaBarScript : MonoBehaviour
+{
+    public Slider slider;
+
+    
+
+    public void SetMana(int mana)
+    {
+        slider.value = mana;
+    }
+
+    public void SetMaxMana(int maxMana)
+    {
+        slider.maxValue = maxMana;
+        slider.value = maxMana;
+    }
+
+    void Start()
+    {
+        slider = GetComponent<Slider>();
+        if(slider == null)
+        {
+            Debug.Log("No Slider attached to ManaBar!");
+        }
+    }
+}
